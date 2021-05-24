d7308761cf0f4082042d75e31ed038ea543c2693
lljose.mnl@eac.edu.ph <lljose.mnl@eac.edu.ph>
2021-05-24 02:59:11 +0000

    File generated by Skaffolder - user: lljose.mnl@eac.edu.ph

---
+++ server/classes/Database_StudentManagement_db.js
@@ -0,0 +1,58 @@
+// Import Mongoose
+import mongoose from "mongoose";
+// Logging
+import Logger from "./Logger";
+// Properties
+import properties from "../properties.js";
+
+// Start Import Models
+
+import StudentModel from "../models/StudentManagement_db/StudentModel";
+import UserModel from "../models/StudentManagement_db/UserModel";
+
+// End Import Models
+
+class Database {
+  constructor() {}
+
+  /**
+   * Init database
+   */
+  async init() {
+    await this.authenticate();
+    Logger.info("MongoDB connected at: " + properties.StudentManagement_db_dbUrl);
+
+    // Start Init Models
+
+		StudentModel.init();
+		UserModel.init();
+ // End Init Models
+  }
+
+  /**
+   * Start database connection
+   */
+  async authenticate() {
+    Logger.info("Authenticating to the databases...");
+    try {
+      this.dbConnection_StudentManagement_db = await mongoose.connect(
+        "mongodb://" + properties.StudentManagement_db_dbUrl,
+        { useNewUrlParser: true }
+      );
+    } catch (err) {
+      Logger.error(`Failed connection to the DB: ${err.message}`);
+      Logger.error(err);
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await this.authenticate();
+    }
+  }
+
+  /**
+   * Get connection db
+   */
+  getConnection() {
+    return this.dbConnection_StudentManagement_db;
+  }
+}
+
+export default new Database();
---
+++ server/properties.js
@@ -0,0 +1,7 @@
+module.exports = {
+	StudentManagement_db_dbUrl: (process.env.DB_HOST || 'localhost') + ':27017/StudentManagement_db',
+    publicPath: '../client/build',
+	port: process.env.NODE_PORT || 3000,
+    tokenSecret: 'Insert Your Secret Token',
+    api: process.env.NODE_API != null ? process.env.NODE_API : '/api'
+}
